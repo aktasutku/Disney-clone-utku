9a75119ce0b75149f5ec93f0f48debfc7c1b117b
aktasutku <101160301+aktasutku@users.noreply.github.com>
2022-06-12 00:12:29 -0400

    Add files via upload
    Api key updated

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db=getFirestore(app)
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+export {auth , provider , storage , analytics}
+export default db
